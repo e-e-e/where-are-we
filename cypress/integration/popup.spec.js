bbae38680bea22c9b131f458f06fc050e322d231
Benjamin Forster <benjamin.forster@reaktor.com>
2020-12-01 22:32:02 +0200

    fix e2e tests

--- cypress/integration/popup.spec.js
+++ cypress/integration/popup.spec.js
@@ -23,7 +23,7 @@ describe('Where Are We - Popup', () => {
     it('Shows a popup with no known indigenous territories', () => {
       cy.contains(
         'div',
-        'Your IP indicates that you may be in Helsinki, Uusimaa, FI.'
+        'Your IP suggests that you may be in Helsinki, Uusimaa, FI. We cannot find information about indigenous nations at this location, this does not mean that there are not any.'
       )
       cy.percySnapshot('Where we are (Helsinki)', { widths: [768, 992, 1200] })
     })
@@ -40,7 +40,7 @@ describe('Where Are We - Popup', () => {
     it('Show a popup with a singular known indigenous territory', () => {
       cy.contains(
         'div',
-        'Your IP indicates that you may be on Wajuk country, otherwise known as Perth, Western Australia, AU.'
+        'Your IP suggests that you may be on Wajuk country, otherwise known as Perth, Western Australia, AU.'
       )
       cy.percySnapshot('Where we are (Perth)', { widths: [768, 992, 1200] })
     })
@@ -57,7 +57,7 @@ describe('Where Are We - Popup', () => {
     it('Show a popup with multiple possible indigenous territories', () => {
       cy.contains(
         'div',
-        'Your IP indicates that you may be on Ho-de-no-sau-nee-ga (Haudenosaunee), Anishinabewaki ᐊᓂᔑᓈᐯᐗᑭ, Mississaugas of the Credit First Nation, and/or Mississauga country, otherwise known as Toronto, Ontario, CA.'
+        'Your IP suggests that you may be on Ho-de-no-sau-nee-ga (Haudenosaunee), Anishinabewaki ᐊᓂᔑᓈᐯᐗᑭ, Mississaugas of the Credit First Nation, and/or Mississauga country, otherwise known as Toronto, Ontario, CA.'
       )
       cy.percySnapshot('Where we are (Toronto)', { widths: [768, 992, 1200] })
     })
